c7cb98cf1cfe396fe1546381530f876b25899034
Joel <joepotential@gmail.com>
2024-03-29 22:03:12 +0100

    mobile header done

--- app.js
+++ app.js
@@ -29,3 +29,37 @@ x.forEach(function(btn) {
         window.location = "https://www.x.com/"
     })
 })
+
+
+document.getElementById("contactForm").addEventListener("submit", function(event) {
+    event.preventDefault(); // Prevent default form submission
+    
+    // Get form data
+    var formData = new FormData(this);
+    
+    // Send form data asynchronously
+    fetch('https://formspree.io/f/xgegnkzl', {
+    method: 'POST',
+    body: formData,
+    headers: {
+        'Accept': 'application/json'
+        }
+    })
+    .then(response => {
+        if (!response.ok) {
+        throw new Error('Network response was not ok');
+        }
+        return response.json();
+    })
+    .then(data => {
+    // Handle successful form submission
+    alert('Thank you! Your message has been sent.');
+    // Optionally, redirect users to another page
+    window.location.href = 'thank-you.html'; // Replace 'thank-you.html' with your thank you page URL
+    })
+    .catch(error => {
+    // Handle errors
+    console.error('There was an error!', error);
+    alert('Oops! Something went wrong.');
+        });
+});
